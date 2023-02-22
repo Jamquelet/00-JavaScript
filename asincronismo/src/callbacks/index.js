c02ad6f4fa04d03c3e063be3489197488ec1f1ea
Jose Maya <jamquelet@hotmail.com>
2023-02-21 21:55:14 -0500

    callback, XMLHttpRequest

---
+++ asincronismo/src/callbacks/index.js
@@ -0,0 +1,44 @@
+function sum(num1,num2) {
+    return num1 + num2;
+}
+
+function calc (num1,num2,callback){
+    return callback(num1,num2)
+}
+
+console.log(calc(2, 2, sum));
+//
+setTimeout(function () {
+    console.log('hi js');
+}, 5000)
+
+function gretting(name) {
+    console.log(`hello ${name}`);
+}
+setTimeout(gretting, 2000, 'Jam');
+//exercice chatGpt
+function execCallback(callback) {
+    setTimeout(function() {
+      window[callback.name] = callback; // Agregamos la función al namespace window
+      callback(); // Llamamos a la función callback
+    }, 2000); // Demora de 2 segundos
+  }
+
+//exercise ejecucion de pruebas
+function execCallback(callback) {
+    window.setTimeout(() => {
+      callback();
+    }, 2000);
+  }
+
+  function execCallback(callback) {
+    setTimeout(() => {
+      callback();
+    }, 2000);
+  }
+  
+  function showMessage() {
+    console.log("Han pasado 2 segundos");
+  }
+  
+  execCallback(showMessage);
